bc6874aeeb503120c694a43bf2f2d7f54ffbdfde
Nazim Huseynov <333nazim333@gmail.com>
2023-01-18 22:13:05 +0400

    updated python script

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route('/')
+def hello_from_nazim():
+    return "<b><font color=blue>Hello GitHub Actions World from Nazim Huseynov!</font></b>"
+
+@application.route('/aboutme')
+def aboutme():
+    return "<b><font color=green>My name is Nazim. I am a Cloud/DevOps Engineer!</font></b>"
+
+if __name__ == "__main__" 
+    application.run()
